9da7957b3c28aa20d480dafc9f04a287dc8be942
Andrew J. DiGiacomo <andrewdigiacomo@storm.cis.fordham.edu>
2016-02-08 21:00:52 -0500

    whawtever you want to call it

---
+++ CISC1600/Homeworks/HW2.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main ()
+{
+	string name;
+	cout << "Hi, my name is Andrew. I am 20 years old, and my favorite thing is money. " << endl;
+	return 0;
+}
+	
+	
